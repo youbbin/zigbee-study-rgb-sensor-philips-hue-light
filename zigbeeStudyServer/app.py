55d4beec213c5a1d9a2a3e3dd32fd670859a1df8
youbbin <youbinjinyoung@gmail.com>
2022-12-30 14:14:59 +0900

    주석 추가

--- zigbeeStudyServer/app.py
+++ zigbeeStudyServer/app.py
@@ -22,37 +22,25 @@ def xbeeReceive(): # 데이터 수신 함수
         while True:
             xbee_message = device.read_data()  # 데이터 읽기
             if xbee_message is not None:
-                data_receive = xbee_message.data.decode()
+                data_receive = xbee_message.data.decode()   # xbee 데이터 디코딩
                 print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(), data_receive))
                 global data_json
-                data_json = json.loads(data_receive)
-                # global r
-                # r= str(data_json['cct'])
-       # def data_receive_callback(xbee_message): # 데이터 수신 콜백 함수
-        #     data = xbee_message.data.decode()
-        #     print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
-        #                              data))
-        #
-        #     data_json = json.loads(data)
-        #     r=data_json['cct']
-
-        # device.add_data_received_callback(data_receive_callback)
+                data_json = json.loads(data_receive) # json 형식으로 저장
 
         print("데이터 기다리는 중...\n")
-
 
     finally:
         if device is not None and device.is_open():
             device.close()
 
 @app.route('/')
-def startXbee():
+def startXbee(): # 클라이언트 접속 시 xbee 데이터 수신 시작
     Response(xbeeReceive())
     return 'OK'
 
 @app.route('/rgbSensorValue', methods = ['GET'])
 def rgbsensor_get():
-    return data_json
+    return data_json # json 형식의 광특성 데이터 리턴
 
 
 if __name__ == '__main__':
